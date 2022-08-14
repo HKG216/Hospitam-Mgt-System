90d5c8ff1a0514beaed8b50f3b6c556a51b5c349
hamseabdi03 <xamsecabdi2003@gmail.com>
2022-08-14 13:13:59 +0300

    Ready Project

---
+++ SYSTEM/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SYSTEM.Models;
+
+namespace SYSTEM.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SYSTEM.Models.ApplicationUser>? ApplicationUser { get; set; }
+    }
+}
---
+++ SYSTEM/Models/RoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace SYSTEM.Models
+{
+    public class RoleViewModel
+    {
+        [Key]
+        public string? Name { get; set; }
+        public string? NormalizedName { get; set; }
+    }
+}
